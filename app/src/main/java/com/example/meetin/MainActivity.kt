43ab94248c07e20d22296b6461a564463653b491
Kanishk <kanishkchhabra23@gmail.com>
2021-02-22 22:12:19 +0530

    Changes in Main Activity

--- app/src/main/java/com/example/meetin/MainActivity.kt
+++ app/src/main/java/com/example/meetin/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.meetin
 
+import android.app.DownloadManager
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.meetin.doas.PostDao
+import com.example.meetin.models.Post
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.firebase.firestore.Query
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var postDao: PostDao
     private lateinit var adapter: PostAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +29,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setUpRecyclerView() {
+        val postCollections = postDao.postCollections
+        val query = postCollections.orderBy("createdAt", Query.Direction.DESCENDING)
+        val recyclerViewOptions = FirestoreRecyclerOptions.Builder<Post>().setQuery(query, Post::class.java).build()
 
+        adapter = PostAdapter(recyclerViewOptions)
+
+        postRecyclerView.adapter = adapter
+        postRecyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
